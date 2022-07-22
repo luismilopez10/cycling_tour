d82eff01a50345a523dd808e306bda56387ea198
Luis Miguel López <luis.miguel.lopez.10@gmail.com>
2022-07-22 13:25:25 -0500

    Initial commit

---
+++ src/main/java/com/sofka/cyclingtour/CyclingtourApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.cyclingtour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyclingtourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyclingtourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofka/cyclingtour/CyclingtourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.cyclingtour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyclingtourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
